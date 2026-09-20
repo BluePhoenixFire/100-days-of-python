c62c48ecfa9b7387d36884b706c650b9d24c46e4
Blue Phoenix <118826875+BluePhoenixFire@users.noreply.github.com>
2026-09-20 09:41:21 +0100

    Refactor `hirst_painter` and `circle_placer` for improved readability and removed unnecessary parameters.

--- day-18/main_18.py
+++ day-18/main_18.py
@@ -12,12 +12,6 @@ def color_extractor(image_path: str = "image.jpg", number_of_colors: int = 30, w
     return rgb_colors
 
 
-#TODO func hist painter
-# accepts, colorlist, rows, columns, spacing, circle size
-# move turtle bottom left
-# iterate rows by columns dropping circles
-# random.choice - colors
-
 def circle_placer(turt: Turtle, color_list: list[tuple[int,int,int]], circle_size: int = 20) -> None:
     turt.color(choice(color_list))
     turt.dot(circle_size)
@@ -38,7 +32,6 @@ def hirst_painter(turt: Turtle,
         for _ in range(columns):
             circle_placer(turt, colorlist, circle_size)
             turt.forward(spacing)
-
 
 
 def main():
